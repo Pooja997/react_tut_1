f353b1fb16df853fd4e68f24ad46016dd95f0a0f
Pooja Rakshe <poojarakshe99@gmail.com>
2022-12-01 08:35:52 +0000

    commit for, tst1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import EmpItem from "./components/EmpItem";
 function App() {
   const data_arr = [
     { name: "Pooja", age: 25, address: "Solapur/Pune" },
-    { name: "Prajwal", age: 18, address: "Pune" },
+    { name: "Prajwal", age: 18, address: "Pune/Jule Solapur" },
   ];
   return (
     <div>
