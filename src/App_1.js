eae9a6c6b6908f72eb878fbee1761445202fbd35
Pooja Rakshe <poojarakshe99@gmail.com>
2022-12-12 14:34:52 +0000

    temp commit

---
+++ src/App_1.js
@@ -0,0 +1,35 @@
+import "./styles.css";
+import EmpItem from "./components/EmpItem";
+import Card from "./components/Card";
+//import FormElements from "./components/FormElements";
+
+function App1() {
+  const data_arr = [
+    { name: "Pooja", age: 25, address: "Solapur/Pune" },
+    { name: "Prajwal", age: 18, address: "Pune/Jule Solapur" },
+  ];
+
+  return (
+    <div>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+      </div>
+      <h3 className="App textCss">****Emp Details ****</h3>
+
+      <Card>
+        <EmpItem
+          name={data_arr[0].name}
+          address={data_arr[0].address}
+          age={data_arr[0].age}
+        />
+        <EmpItem
+          name={data_arr[1].name}
+          address={data_arr[1].address}
+          age={data_arr[1].age}
+        />
+      </Card>
+    </div>
+  );
+}
+
+export default App1;
--- src/components/FormElements.js
+++ src/components/FormElements.js
@@ -1,14 +1,71 @@
+import React, { useState } from "react";
+
+const btnName = "Submit";
 function FormElements(props) {
-  //const [title, setTitle] = useState(props.name);
+  /* Start : Approach 1 */
+  /*const [fName, setFname] = useState("");
+  const [btnName, setBtnName] = useState("Submit");
+
   function clickHandler() {
-    alert(props.name);
+    setBtnName("Clicked");
+  }
+
+  function changeHandler(event) {
+    console.log(event);
+    setFname(event.target.value);
+  }
+  return (
+    <div>
+      <label>Name : </label>
+      <input type="text" onChange={changeHandler} value={fName} />
+      <br />
+      <br />
+      <button className="btnCls" onClick={clickHandler}>
+        {btnName}
+      </button>
+    </div>
+  );
+}*/
+  /* End : Approach 1 */
+
+  /* Start : Approach 2 */
+  const [userInput, setUserInput] = useState({
+    fName: "",
+    //salary: "setSalary",
+    btnName: "",
+  });
+
+  function setSalary() {}
+
+  function clickHandler() {
+    setBtnName("Clicked");
+  }
+
+  function setFirstName(event) {
+    console.log(event);
+    setUserInput({
+      ...userInput,
+      fName: "Pooja",
+    });
   }
 
   return (
-    <button className="btnCls" onClick={clickHandler}>
-      Update
-    </button>
+    <div>
+      <label>Name : </label>
+      <input type="text" onChange={setFirstName} />
+      <br />
+
+      <label>Salary : </label>
+      <input type="number" onChange={setSalary} />
+      <br />
+
+      <br />
+      <button className="btnCls" onClick={clickHandler}>
+        {btnName}
+      </button>
+    </div>
   );
 }
+/* End : Approach 2 */
 
 export default FormElements;
